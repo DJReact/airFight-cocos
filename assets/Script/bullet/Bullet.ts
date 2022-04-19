84ccf8bb28c5e6b650c115bbe76fe572f5578af1
pengizhiyuan <git config --global user.email>
2022-04-19 18:30:53 +0800

    feat: 代码规范化、好排查错误

--- assets/Script/bullet/Bullet.ts
+++ assets/Script/bullet/Bullet.ts
@@ -1,17 +1,14 @@
-// 子弹控制
 import { _decorator, Component, Node } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass("bulletManager")
-export class bulletManager extends Component {
+@ccclass("Bullet")
+export class Bullet extends Component {
   @property
   public speed = 1;
-  start() {
-    // [3]
-  }
-  update(time) {
+  update() {
     const pos = this.node.position;
-    const moveLength = pos.z - this.speed;
+    // 每帧向前移动
+    let moveLength = pos.z - this.speed;
     this.node.setPosition(pos.x, pos.y, moveLength);
   }
 }
